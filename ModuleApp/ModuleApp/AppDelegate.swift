475aad440d472f994fdd0d881eb6a822af2e0aa7
hyh <576681253@qq.com>
2025-02-21 15:49:15 +0800

    什么是模块化

---
+++ ModuleApp/ModuleApp/AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  ModuleApp
+//
+//  Created by hyh on 2025/2/21.
+//
+
+import UIKit
+
+import ModuleAccountInterface
+import ModuleChatInterface
+import ModuleMomentInterface
+
+import ModuleAccount
+import ModuleChat
+import ModuleMoment
+
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        // 注册模块
+        mRegisterAccountModel()
+        mRegisterChatModel()
+        mRegisterMomentModel()
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
